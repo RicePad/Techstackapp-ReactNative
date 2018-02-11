d5aaee521851c6c138f0634a8a15547a83083af0
JON <jonn.thinkrtc@gmail.com>
2018-02-10 16:38:54 -0800

    added selection reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import LibraryList from './library_list_reducer';
+import SelectionReducer from './selection_reducer';
 
 
 
 export default combineReducers({
-	libraries: LibraryList
+	libraries: LibraryList,
+	selectedLibraryId: SelectionReducer
 })
